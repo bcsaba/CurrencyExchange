a6f5a340fcc68d1485d1f8fe801c4ecb73afdb4f
Csaba Benko <csaba.benko@gmail.com>
2023-10-20 16:25:16 +0200

    Added input UI elements to currency converter page and service to submit convrsion request.

--- CurrencyExchange/ClientApp/src/app/huf-converter/huf-converter.component.ts
+++ CurrencyExchange/ClientApp/src/app/huf-converter/huf-converter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CurrencyConversionModel, ExchangeRatesService} from "../exchange-rates.service";
 
 @Component({
   selector: 'app-huf-converter',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./huf-converter.component.css']
 })
 export class HufConverterComponent {
+  public amount: number = 1;
 
+  constructor(
+    private exchangeRateService: ExchangeRatesService,
+  ) {
+  }
+
+  convert() {
+    let currencyConversionModel: CurrencyConversionModel = {
+      fromCurrency: "HUF",
+      fromAmount: this.amount,
+      toCurrency: "EUR"};
+
+    this.exchangeRateService.convertHufToEur(currencyConversionModel)
+      .subscribe(
+        {
+          complete: () => console.log('HUF conversion completed'),
+          error: err => console.error(err),
+          next: value => {
+            console.log(value);
+          }
+        }
+      );
+  }
 }
