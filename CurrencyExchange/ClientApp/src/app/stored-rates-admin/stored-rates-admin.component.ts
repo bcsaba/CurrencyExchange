75e8531601fa3af29d4f7264b5efadc42a8444fc
Csaba Benko <csaba.benko@gmail.com>
2023-10-20 22:16:55 +0200

    Save of updated comment works. Refresh after save needs to be fixed.

--- CurrencyExchange/ClientApp/src/app/stored-rates-admin/stored-rates-admin.component.ts
+++ CurrencyExchange/ClientApp/src/app/stored-rates-admin/stored-rates-admin.component.ts
@@ -1,25 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, TemplateRef} from '@angular/core';
 import {ExchangeRates, ExchangeRatesService, RateWithComment} from "../exchange-rates.service";
+import {ToastService} from "../toast.service";
 
 @Component({
   selector: 'app-stored-rates-admin',
   templateUrl: './stored-rates-admin.component.html',
   styleUrls: ['./stored-rates-admin.component.css']
 })
-export class StoredRatesAdminComponent {
-  public storedRates: RateWithComment[] = [];
+export class StoredRatesAdminComponent implements OnDestroy {
+  public storedRates: StoredDateEdit[] = [];
 
-  constructor(private exchangeRatesService: ExchangeRatesService) {
+  constructor(
+    private exchangeRatesService: ExchangeRatesService,
+    private toastService: ToastService
+  ) {
     this.exchangeRatesService.getStoredRates()
       .subscribe(
         {
           complete: () => console.log('Get stored rates completed'),
           error: err => console.error(err),
           next: value => {
-            this.storedRates = value;
+            this.storedRates = value.map(r =>
+              ({...r, editMode: false, initialComment: r.comment}));
             console.log(value);
           }
         },
       )
   }
+
+  updateStoredRate(exchangeRate: StoredDateEdit) {
+    this.exchangeRatesService.saveRateWithComment(exchangeRate)
+      .subscribe(
+        {
+          complete: () => console.log('Update stored rate completed'),
+          error: err => console.error(err),
+          next: value => {
+            this.showSuccess(`Exchange rate for ${value.currency} updated successfully`);
+            let index = this.storedRates.findIndex(
+              r => r.currency === value.currency && r.exchangeDate === value.exchangeDate);
+            if (index >= 0) {
+              console.log(`replacing item (${value.currency}, ${value.exchangeDate})`);
+              this.storedRates[index] = {...value, editMode: false, initialComment: value.comment};
+            }
+            console.log(value);
+          }
+        },
+      )
+  }
+
+  showSuccess(textOrTpl: string | TemplateRef<any>) {
+    this.toastService.show(textOrTpl, { classname: 'bg-success text-light', delay: 10000 });
+  }
+
+  showDanger(textOrTpl: string | TemplateRef<any>) {
+    this.toastService.show(textOrTpl, { classname: 'bg-danger text-light', delay: 15000 });
+  }
+
+  ngOnDestroy(): void {
+    this.toastService.clear();
+  }
 }
+
+interface StoredDateEdit extends RateWithComment {
+  editMode: boolean;
+  initialComment: string;
+};
